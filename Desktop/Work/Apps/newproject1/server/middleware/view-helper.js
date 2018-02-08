5075cf38d872a8028129517882ce67f8f1c02c96
Damian Buttle <damianbuttle@gmail.com>
2018-02-08 17:53:39 -0600

    Initial Commit

--- Desktop/Work/Apps/newproject1/server/middleware/view-helper.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var secrets = require('../config/secrets');
-
-module.exports = function(req, res, next) {
-  res.locals.path = req.path;
-  res.locals.googleAnalytics = secrets.googleAnalytics;
-  next();
-};
